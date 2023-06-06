791ff6fd170236ff2e1a4de05a8f21ed31ca8a3b
Md. Wahiduzzaman Nayem <nayemwahid05@gmail.com>
2023-06-07 00:20:37 +0600

    ভ

---
+++ TreeV/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from django.conf import settings
+from .views import *
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('products', products, name='products'),
+    path('login', login, name='login'),
+    path('signup', signup, name='signup'),
+    path('register', register, name='register'),
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
+    path('reviews', reviews, name='reviews'),
+    path('cart', cart, name='cart'),
+    path('addToCart/<str:pname>/<int:amt>/', addToCart, name='addToCart'),
+    path('checkSignin', checkSignin, name='checkSignin'),
+]
